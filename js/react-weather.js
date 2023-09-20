de6ec82c76a31343ab9e4d96223db4c8cede4d12
Mara's Mac <williamsmc@my.lanecc.edu>
2023-09-20 12:49:30 -0700

    style changes, new sans serif font

--- js/react-weather.js
+++ js/react-weather.js
@@ -9,6 +9,8 @@ class DetailPage {
             image:"react-weather.jpg",
             linkText:"Visit",
             link: "http://citstudent.lanecc.net/~williamsm312/react-class/weather/",
+            github: "https://github.com/maracw/react-weather",
+            githubLinkText: "View Source Code on Github",
             description:["This is one of the first React pages I made. A user can enter a 5-digit US zipcode and see weather data in that location for the next 4 days.",
             "The styling is a combination of bootstrap and custom CSS"]
         };
@@ -23,11 +25,17 @@ class DetailPage {
             document.getElementById("project-desc").innerHTML=this.createDescription(this.currentProject.description);
         }
         createProjectHeaderInnerHtml(project){
-            const projectheaderString=`<h1 id="project-title">${project.title}</h1>
+            const projectheaderString=`
+            <h1 id="project-title">${project.title}</h1>
             <h3 id="project-subtitle">${project.subtitle}</h3>
+            <div>
                 <div id="visit-btn">
                     <a class="my-button" href="${project.link}" target="_blank">${project.linkText}</a></li>
-                 </div>`
+                </div>
+                <div id="visit-btn">
+                    <a class="my-button" href="${project.github}" target="_blank">${project.githubLinkText}</a></li>
+                </div>
+            <div>`
             return projectheaderString;
         }
         createProjectImageInnerHtml (project) {
